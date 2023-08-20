c717c6ab58e3a6dec524bef00be01e3614752c93
alvitoaditya19 <you@example.com>
2023-08-20 07:11:44 +0700

    updated code v5

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:safeguarduser/shared/theme.dart';
 import 'package:sensors/sensors.dart';
 import 'package:flutter/services.dart';
 import 'package:vibration/vibration.dart';
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Location Example',
+      debugShowCheckedModeBanner: false,
       home: LocationPage(),
     );
   }
@@ -39,35 +41,61 @@ class _LocationPageState extends State<LocationPage> {
   double _y = 0.0;
   double _z = 0.0;
   double a = 0.0;
-  String pesan = "Pasien Aman";
+  String pesan = "Patient Safe";
+  String _applicationStatus = "Application Active";
 
   String? _currentAddress;
   Position? _currentPosition;
   final DatabaseReference _databaseReference =
       FirebaseDatabase.instance.reference().child('data');
-  
+  bool _isAccelerometerActive = false;
+
   @override
   void initState() {
     super.initState();
- _getCurrentPosition();
-    accelerometerEvents.listen((AccelerometerEvent event) {
-      setState(() {
-        _x = event.x;
-        _y = event.y;
-        _z = event.z;
-        if (_y < 3 && _x >= -2.5 && _x <= 1) {
-          a++;
-          if (a >= 15) {
-            _sendLocationToFirebase(
-                _currentPosition?.latitude, _currentPosition?.longitude);
-            _startVibration();
-            pesan = "Pasien Terjatuh";
-            a = 0;
-          }
-        } else {
-          a = 0; // Reset the counter if conditions are not met
-        }
-      });
+    _isAccelerometerActive = false;
+    // if (!_isAccelerometerActive) {
+    //   _getCurrentPosition();
+
+    //   accelerometerEvents.listen((AccelerometerEvent event) {
+    //     setState(() {
+    //       _x = event.x;
+    //       _y = event.y;
+    //       _z = event.z;
+    //       if (_y < 3 && _x >= -2.5 && _x <= 1) {
+    //         a++;
+    //         if (a >= 15) {
+    //           _sendLocationToFirebase(
+    //               _currentPosition?.latitude, _currentPosition?.longitude);
+    //           _startVibration();
+    //           pesan = "Alert !!! Patient Fallen";
+    //           a = 0;
+    //         }
+    //       } else {
+    //         a = 0; // Reset the counter if conditions are not met
+    //         setState(() {
+    //           _isVibrating = false;
+    //         });
+    //         pesan = "Patient Safe";
+    //       }
+    //     });
+    //   });
+    // } else {
+    //   accelerometerEvents.listen((AccelerometerEvent event) {
+    //     _stopVibration();
+    //     setState(() {
+    //       _isVibrating = false;
+    //     });
+    //   });
+    // }
+  }
+
+  void _stopAccelerometer() {
+    _stopVibration();
+    setState(() {
+      a = 0;
+      pesan = "Patient Safe";
+      _isAccelerometerActive = false;
     });
   }
 
@@ -127,13 +155,18 @@ class _LocationPageState extends State<LocationPage> {
     });
   }
 
-  void _sendLocationToFirebase(double? latitude, double? longitude) async {
+  void _sendLocationToFirebase(
+      double? latitude, double? longitude, String? address) async {
     final mapsUrl =
         'https://maps.app.goo.gl/?link=https://www.google.com/maps/place/$latitude,$longitude';
     _databaseReference.update({
       'address': mapsUrl,
-      'name': "vcvcv",
-      'status': "54ddd",
+      'name': "Patient",
+      'status': "yes",
+      'timestamp': DateTime.now().toString(),
+      'position': address,
+      'latitude': latitude,
+      'longitude': longitude
     });
   }
 
@@ -154,7 +187,7 @@ class _LocationPageState extends State<LocationPage> {
   void _vibrateLoop() {
     if (_isVibrating) {
       Vibration.vibrate(duration: 1000);
-      Future.delayed(Duration(seconds: 1), () {
+      Future.delayed(Duration(milliseconds: 400), () {
         if (_isVibrating) {
           _vibrateLoop();
         }
@@ -162,53 +195,261 @@ class _LocationPageState extends State<LocationPage> {
     }
   }
 
+  Future<void> _showStatusAlertDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Confirmation Status',
+            style: blackTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          content: Text('Have you treated the patient?',
+              style: blackTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: medium,
+              )),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                'Yes',
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              onPressed: () {
+                _stopAccelerometer();
+                Map<String, dynamic> updatedData = {
+                  'status': 'no',
+                };
+                a = 0;
+                _databaseReference.update(updatedData).then((_) {
+                  print("Data updated successfully");
+                }).catchError((error) {
+                  print("Failed to update data: $error");
+                });
+                setState(() {
+                  _isAccelerometerActive = false;
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _toggleAccelerometer() {
+    setState(() {
+      // Toggle _isAccelerometerActive status
+      _isAccelerometerActive = !_isAccelerometerActive;
+
+      if (_isAccelerometerActive) {
+        // Start listening to accelerometer events
+        _getCurrentPosition();
+        accelerometerEvents.listen((AccelerometerEvent event) {
+          setState(() {
+            _x = event.x;
+            _y = event.y;
+            _z = event.z;
+            if (_y < 3 && _x >= -2.5 && _x <= 1) {
+              a++;
+              if (a >= 15) {
+                _sendLocationToFirebase(_currentPosition?.latitude,
+                    _currentPosition?.longitude, _currentAddress);
+                _startVibration();
+                _showStatusAlertDialog();
+                pesan = "Alert !!! Patient Fallen";
+                a = 0;
+              }
+            } else {
+              a = 0; // Reset the counter if conditions are not met
+              setState(() {
+                _isVibrating = false;
+              });
+              pesan = "Patient Safe";
+            }
+          });
+        });
+      } else {
+        // Stop listening to accelerometer events and vibration
+        accelerometerEvents.listen((AccelerometerEvent event) {
+          a = 0; // Reset the counter if conditions are not met
+
+          _stopVibration();
+          setState(() {
+            _isVibrating = false;
+          });
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Location Page")),
-      body: SafeArea(
-        child: Center(
+      appBar: AppBar(
+          backgroundColor: greenColor,
+          title: Text(
+            "Safeguard Sensor",
+            style: whiteTextStyle.copyWith(
+              fontSize: 20,
+              fontWeight: semiBold,
+            ),
+          )),
+      body: SingleChildScrollView(
+        child: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text('LAT: ${_currentPosition?.latitude ?? ""}'),
-              Text('LNG: ${_currentPosition?.longitude ?? ""}'),
-              Text('ADDRESS: ${_currentAddress ?? ""}'),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: _getCurrentPosition,
-                child: const Text("Get Current Location"),
+              SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: Text(
+                  "Would you like to activate this application?",
+                  style: blackTextStyle.copyWith(
+                    fontSize: 20,
+                    fontWeight: semiBold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 16,
               ),
               ElevatedButton(
-                onPressed: () => _sendLocationToFirebase(
-                    _currentPosition?.latitude, _currentPosition?.longitude),
-                child: const Text("Send Location to Firebase"),
+                onPressed: _toggleAccelerometer,
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  primary: _isAccelerometerActive
+                      ? redColor
+                      : greenColor, // Set your desired background color here
+                ),
+                child: Text(
+                  _isAccelerometerActive ? 'Turn OFF' : 'Turn ON',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 20,
+                    fontWeight: semiBold,
+                  ),
+                ),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Accelerometer Data'),
-                  SizedBox(height: 16.0),
-                  Text('X: $_x'),
-                  SizedBox(height: 16.0),
-                  Text('Y: $_y'),
-                  SizedBox(height: 16.0),
-                  Text('Z: $_z'),
-                  SizedBox(height: 50.0),
-                  Text('Counting: $a'),
-                  SizedBox(height: 50.0),
-                  Text('Pesan: $pesan'),
-                ],
+              SizedBox(
+                height: 16,
               ),
-              _isVibrating
-                  ? ElevatedButton(
-                      onPressed: _stopVibration,
-                      child: Text('Stop Vibration'),
+              _isAccelerometerActive
+                  ? Container(
+                      margin: EdgeInsets.symmetric(horizontal: 24),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Data Location',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 22,
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Latitude: ${_currentPosition?.latitude ?? ""}',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Longitude: ${_currentPosition?.longitude ?? ""}',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Address: ${_currentAddress ?? ""}',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'Accelerometer Sensor Data',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 22,
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            'X: $_x',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            'Y: $_y',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            'Z: $_z',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            'Counting: $a',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                          SizedBox(height: 16.0),
+                          Text(
+                            'Data Location',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 22,
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                          SizedBox(height: 12.0),
+                          Text(
+                            'Pesan: $pesan',
+                            style: blackTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                        ],
+                      ),
                     )
-                  : ElevatedButton(
-                      onPressed: _startVibration,
-                      child: Text('Start Vibration'),
-                    ),
+                  : SizedBox(),
             ],
           ),
         ),
